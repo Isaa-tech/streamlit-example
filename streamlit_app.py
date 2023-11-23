0d2ce4d2bbadb6a3a7ce9bd3ef5c61f8e36280b6
Isaa-tech <105975822+Isaa-tech@users.noreply.github.com>
2023-11-23 14:22:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,43 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
+st.write("## Dashboard Tutorial") # ## für Ueberschrift Anzeige 
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+df = pd.read_csv("Boardgame.csv", sep=";", index_col= 1)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+Top_ten = df.head(10)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+text = st.text_input("Spielname: ", value="")
+st.write("**" + text + "**")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+Treffer = df[df.index.str.contains(text)]
+st.write(Treffer)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+# Filtern nach Spieler anzahl
+options = ["2", "3", "4", "5", "6"] # Auswahl Möglichkeiten in der Dropdown Liste
+col_name = st.selectbox("Wähle eine Maximale Spieleranzahl", options)  
+#st.write(col_name) # schreibt es auf dem Schirm 
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+tab1, tab2 = st.tabs(["Bar Chart", "Scatter Chart"])
+
+show_plots = st.toggle('Show some plots')
+if show_plots:
+    df = df[df["Max Players"] == int(col_name)]
+    st.write(df.shape) # Anzahl Zeilen + Spalten
+    
+    Top_ten = df.head(10)
+    st.write(Top_ten)
+    with tab1:
+        st.bar_chart(Top_ten["Owned Users"])
+    with tab2:
+        st.scatter_chart(
+        data=df, 
+        x="Year Published", 
+        y=["Min Players", "Max Players"], # Eckige Klammern, damit mehrere Linien angezeigt werden
+        color=["#00008B", "#CD3333"],       # Größe in Pixeln (0= defualt)
+        width=0, height=0,
+        use_container_width=True)
+    
+else:
+    st.write("Kein Plot vorhanden")
